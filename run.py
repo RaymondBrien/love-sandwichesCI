bd59defb4b64f61413d088a3c596f4a09d802444
Raymond <raytbrien@gmail.com>
2024-02-07 16:50:11 +0000

    Calculate surplus data with full functionality

--- run.py
+++ run.py
@@ -73,7 +73,14 @@ def calculate_surplus_data(sales_row):
     print('Calculating surplus data...\n ')
     stock = SHEET.worksheet("stock").get_all_values() 
     stock_row = stock[-1]
-    pprint(stock_row)
+
+    surplus_data = []
+    
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
 
 
     print('Surplus data calculated!\n')
@@ -87,7 +94,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data] #convert all sales data to integers to be accepte by google sheets
     update_sales_data(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 print('Welcome to Love Sandwiches!\n')
 main()
