24b0e7176603e185d567f83327e3f94ace81a6b4
Raymond <raytbrien@gmail.com>
2024-02-08 14:29:48 +0000

    Calculate stock for next day and add to worksheet

--- run.py
+++ run.py
@@ -86,7 +86,7 @@ def update_worksheet(data, worksheet):
 
 def get_last_five_entries_sales():
     """
-    Gets last 5 entries of sales data, returns mean average plus 10% for sales incentive.
+    Gets last 5 entries of sales data
     """
     sales = SHEET.worksheet("sales")
     columns = []
@@ -94,6 +94,20 @@ def get_last_five_entries_sales():
         column = sales.col_values(ind)
         columns.append(column[-5:])
     return columns 
+
+def calculate_stock_data(data):
+    """
+    Returns mean average of last 5 sales entries, plus 10% for sales incentive.
+    """
+    print('Calculating stock data...\n')
+    new_stock_data = []
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = int(average) * 1.1
+        new_stock_data.append(round(stock_num))
+    return new_stock_data
+    
 def main():
     """
     Call all program functions
@@ -103,8 +117,11 @@ def main():
     update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, 'surplus')
+    sales_columns = get_last_five_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data,'stock')
 
 print('Welcome to Love Sandwiches!\n')
-# main()
-sales_columns = get_last_five_entries_sales()
+main()
+
  
